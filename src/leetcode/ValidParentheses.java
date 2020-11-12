3a6c1b5d0403afc4442daa771ffd82cbc7aba5ac
yacolin <mcolinqi@sina.com>
2020-11-12 17:48:35 +0800

    ValidParentheses

---
+++ src/leetcode/ValidParentheses.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '[')
+                stack.push(']');
+            else if (c == '{')
+                stack.push('}');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String[] strs = new String[] {"()[]{}", "(({{}}))", "(([[]]}"};
+
+        for (String str : strs)
+            System.out.printf("%s = %s%n", str, isValid(str));
+    }
+}
